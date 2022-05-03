b5e071a3fb103e11d87d184c0b83f62ab8ad8792
SavioCastellar <78110795+SavioCastellar@users.noreply.github.com>
2022-05-03 18:10:12 -0300

    sprint2

--- forca.py
+++ forca.py
@@ -3,7 +3,20 @@ def jogar():
     print("Bem-vindo ao jogo de forca!")
     print("================================")
 
-    palavra_secreta
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        i = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("A letra {} está na posição {}".format(letra, i))
+            i = i + 1
+
 
     print("Fim do jogo.")
 
