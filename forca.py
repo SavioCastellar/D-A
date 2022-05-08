6cb69df33d3b26337f5acef111861a70ebfa4031
SavioCastellar <78110795+SavioCastellar@users.noreply.github.com>
2022-05-08 10:29:48 -0300

    Sprint2

--- forca.py
+++ forca.py
@@ -1,10 +1,38 @@
 import random
 
+
 def jogar():
+    mensagem_abertura()
+    palavra_secreta = gera_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while not enforcou and not acertou:
+        chute = solicita_chute()
+
+        if chute in palavra_secreta:
+            verifica_chute(palavra_secreta, letras_acertadas, chute)
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor()
+
+def mensagem_abertura():
     print("================================")
     print("Bem-vindo ao jogo de forca!")
     print("================================")
 
+def gera_palavra_secreta():
     arquivo = open("palavras.txt", "r")
     palavras = []
 
@@ -16,35 +44,28 @@ def jogar():
 
     numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero].upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    return palavra_secreta
 
-    enforcou = False
-    acertou = False
-    erros = 0
+def inicializa_letras_acertadas(palavra_secreta):
+    return ["_" for letra in palavra_secreta]
 
-    while not enforcou and not acertou:
+def solicita_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+    return chute
 
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
+def verifica_chute(palavra_secreta, letras_acertadas, chute):
+    i = 0
+    for letra in palavra_secreta:
+        if chute.upper() == letra.upper():
+            letras_acertadas[i] = letra
+        i += 1
 
-        if chute in palavra_secreta:
-            i = 0
-            for letra in palavra_secreta:
-                if chute.upper() == letra.upper():
-                    letras_acertadas[i] = letra
-                i += 1
-        else:
-            erros += 1
+def imprime_mensagem_vencedor():
+    print("Você venceu!")
 
-        enforcou = erros == 6
-        acertou = "_" not in letras_acertadas
-        print(letras_acertadas)
-
-    if acertou:
-        print("Você venceu!")
-    else:
-        print("Você perdeu.")
-    print("Fim do jogo.")
+def imprime_mensagem_perdedor():
+    print("Você perdeu!")
 
 if(__name__ == "__main__"):
     jogar()
